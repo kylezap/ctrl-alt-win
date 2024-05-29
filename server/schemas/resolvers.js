86b5034581cd702beb035c345aadb520e1eab037
kylezap <kzapcic+github@gmail.com>
2024-05-29 11:12:53 -0400

    build resolver queries

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,28 +1,57 @@
-const { Tech, Matchup } = require('../models');
+const User = require("./models/User");
+const Game = require("./models/Game");
 
 const resolvers = {
   Query: {
-    tech: async () => {
-      return Tech.find({});
+    getUsers: async () => {
+      const allUsers = await User.find({});
+      if (!allUsers) {
+        throw new Error("No users found");
+      } 
+      return allUsers;
     },
-    matchups: async (parent, { _id }) => {
-      const params = _id ? { _id } : {};
-      return Matchup.find(params);
+    getSingleUser: async (_, { id }) => {
+      const foundUser = await User.findById(id);
+      if (!foundUser) {
+        throw new Error(`No user found with id: ${id}`);
+      } 
+      return foundUser;
     },
+    getGames: async () => {
+      const allGames = await Game.find({});
+      if (!allGames) {
+        throw new Error("No games found");
+      } 
+      return allGames;
+    },
+    getSingleGame: async (_, { id }) => {
+      const foundGame = await Game.findById(id);
+      if (!foundGame) {
+        throw new Error(`No game found with id: ${id}`);
+      }
+      return foundGame;
   },
+  },
+
   Mutation: {
-    createMatchup: async (parent, args) => {
-      const matchup = await Matchup.create(args);
-      return matchup;
-    },
-    createVote: async (parent, { _id, techNum }) => {
-      const vote = await Matchup.findOneAndUpdate(
-        { _id },
-        { $inc: { [`tech${techNum}_votes`]: 1 } },
+    createUser: (_, { firstName, lastName, username, email }) =>
+      User.create({ firstName, lastName, username, email }),
+    updateUser: (_, { id, firstName, lastName, username, email }) =>
+      User.findByIdAndUpdate(
+        id,
+        { firstName, lastName, username, email },
         { new: true }
-      );
-      return vote;
-    },
+      ),
+    deleteUser: (_, { id }) => User.findByIdAndRemove(id),
+    createGame: (_, { name, rating, yearRelease, platform, summary }) =>
+      Game.create({ name, rating, yearRelease, platform, summary }),
+    updateGame: (_, { id, name, rating, yearRelease, platform, summary }) =>
+      Game.findByIdAndUpdate(
+        id,
+        { name, rating, yearRelease, platform, summary },
+        { new: true }
+      ),
+    deleteGame: (_, { id }) => Game.findByIdAndRemove(id),
   },
 };
 
