b312ea9538c99b63985c0330686f95736d6ea09f
kylezap <kzapcic+github@gmail.com>
2024-05-29 11:23:05 -0400

    build mutations file

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,23 +1,67 @@
 import { gql } from '@apollo/client';
 
-export const CREATE_MATCHUP = gql`
-  mutation createMatchup($tech1: String!, $tech2: String!) {
-    createMatchup(tech1: $tech1, tech2: $tech2) {
-      _id
-      tech1
-      tech2
+export const CREATE_USER = gql`
+  mutation CreateUser($firstName: String!, $lastName: String!, $username: String!, $email: String!) {
+    createUser(firstName: $firstName, lastName: $lastName, username: $username, email: $email) {
+      id
+      firstName
+      lastName
+      username
+      email
     }
   }
 `;
 
-export const CREATE_VOTE = gql`
-  mutation createVote($_id: String!, $techNum: Int!) {
-    createVote(_id: $_id, techNum: $techNum) {
-      _id
-      tech1
-      tech2
-      tech1_votes
-      tech2_votes
+export const UPDATE_USER = gql`
+  mutation UpdateUser($id: ID!, $firstName: String, $lastName: String, $username: String, $email: String) {
+    updateUser(id: $id, firstName: $firstName, lastName: $lastName, username: $username, email: $email) {
+      id
+      firstName
+      lastName
+      username
+      email
+    }
+  }
+`;
+
+export const DELETE_USER = gql`
+  mutation DeleteUser($id: ID!) {
+    deleteUser(id: $id) {
+      id
+    }
+  }
+`;
+
+export const CREATE_GAME = gql`
+  mutation CreateGame($name: String!, $rating: String!, $yearRelease: Int, $platform: String, $summary: String) {
+    createGame(name: $name, rating: $rating, yearRelease: $yearRelease, platform: $platform, summary: $summary) {
+      id
+      name
+      rating
+      yearRelease
+      platform
+      summary
+    }
+  }
+`;
+
+export const UPDATE_GAME = gql`
+  mutation UpdateGame($id: ID!, $name: String, $rating: String, $yearRelease: Int, $platform: String, $summary: String) {
+    updateGame(id: $id, name: $name, rating: $rating, yearRelease: $yearRelease, platform: $platform, summary: $summary) {
+      id
+      name
+      rating
+      yearRelease
+      platform
+      summary
+    }
+  }
+`;
+
+export const DELETE_GAME = gql`
+  mutation DeleteGame($id: ID!) {
+    deleteGame(id: $id) {
+      id
     }
   }
 `;
